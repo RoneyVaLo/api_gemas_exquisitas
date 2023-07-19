2defa3c1589acb669de7efb0cc8d081d597559a8
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2023-07-18 19:47:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from typing import List, Optional
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'UTN@Tall3er'
+api = Api(app)
+jwt = JWTManager(app)
 
-def main(args=None):
-    # type: (Optional[List[str]]) -> int
-    """This is preserved for old console scripts that may still be referencing
-    it.
+# Importamos los recursos para cada tabla
+from productos import ProductosResource
+from clientes import ClientesResource
+from facturas import FacturasResource
+from inventario import InventarioResource
 
-    For additional details, see https://github.com/pypa/pip/issues/7498.
-    """
-    from pip._internal.utils.entrypoints import _wrapper
+# Agregamos los recursos a las rutas del API
+api.add_resource(ProductosResource, '/productos',
+                 '/productos/<int:producto_id>')
+api.add_resource(ClientesResource, '/clientes', '/clientes/<int:cliente_id>')
+api.add_resource(FacturasResource, '/facturas', '/facturas/<int:factura_id>')
+api.add_resource(InventarioResource, '/inventario',
+                 '/inventario/<int:inventario_id>')
 
-    return _wrapper(args)
+if __name__ == '__main__':
+  app.run(debug=True)
